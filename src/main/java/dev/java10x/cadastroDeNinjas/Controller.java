494afd06a70a36575b684dc85cc3d71d790fc9a0
João Pedro Faustino <jotapefaustino7@gmail.com>
2025-08-14 18:59:00 -0300

    feat: Create my first controller

---
+++ src/main/java/dev/java10x/cadastroDeNinjas/Controller.java
@@ -0,0 +1,14 @@
+package dev.java10x.cadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+    @GetMapping("/boasVindas")
+    public String boasVindas() {
+        return "Esse é meu primeiro Controller";
+    }
+}
