d594577a410e1593e392313b4801f10bd8365013
João Pedro Faustino <jotapefaustino7@gmail.com>
2025-08-19 18:21:08 -0300

    feat: Add Lombok in project

--- src/main/java/dev/java10x/cadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package dev.java10x.cadastroDeNinjas.Missoes;
 
 import dev.java10x.cadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,28 +23,4 @@ public class MissoesModel {
     // Uma missão pode ter vários ninjas
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
-
-    public MissoesModel() {
-    }
-
-    public MissoesModel(String nome, String dificuldade) {
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
 }
