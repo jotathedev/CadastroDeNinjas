4b44527942c7e1a956940648284bf5812bb04034
João Pedro Faustino <jotapefaustino7@gmail.com>
2025-08-25 17:54:53 -0300

    feat: Create Repositorys

---
+++ src/main/java/dev/java10x/cadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -0,0 +1,6 @@
+package dev.java10x.cadastroDeNinjas.Ninjas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
+}
