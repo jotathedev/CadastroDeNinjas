b9dd6b0237bd848e2fd5fb31bbd50aedbd382e27
jotathedev <jotapefaustino7@gmail.com>
2026-04-03 01:32:09 -0300

    feat: Add DTO for hide the entity's implementation

--- src/main/java/dev/java10x/cadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/Ninjas/NinjaService.java
@@ -9,9 +9,11 @@ import java.util.Optional;
 public class NinjaService {
 
     private NinjaRepository ninjaRepository;
+    private NinjaMapper ninjaMapper;
 
-    public NinjaService(NinjaRepository ninjaRepository) {
+    public NinjaService(NinjaRepository ninjaRepository, NinjaMapper ninjaMapper) {
         this.ninjaRepository = ninjaRepository;
+        this.ninjaMapper = ninjaMapper;
     }
 
     public List<NinjaModel> listarNinjas() {
@@ -23,8 +25,10 @@ public class NinjaService {
         return ninjaPorId.orElse(null);
     }
 
-    public NinjaModel criarNinja(NinjaModel ninja) {
-        return ninjaRepository.save(ninja);
+    public NinjaDTO criarNinja(NinjaDTO ninjaDTO) {
+        NinjaModel ninja = new NinjaMapper().map(ninjaDTO);
+        ninja = ninjaRepository.save(ninja);
+        return ninjaMapper.map(ninja);
     }
 
     public void apagarNinjaPorID(Long id) {
